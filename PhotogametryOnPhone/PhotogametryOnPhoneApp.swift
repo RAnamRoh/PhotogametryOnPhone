63c293f8235f1f984c79cb4bc68557a049130fe5
Rakibul Anam <rakibul.anam@brainstation-23.com>
2024-08-21 17:19:14 +0600

    Initial Commit

---
+++ PhotogametryOnPhone/PhotogametryOnPhoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotogametryOnPhoneApp.swift
+//  PhotogametryOnPhone
+//
+//  Created by BS00834 on 21/8/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotogametryOnPhoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
