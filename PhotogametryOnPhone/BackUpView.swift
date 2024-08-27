53822d78ef512255e713a07f4308faeb238a634d
Rakibul Anam <rakibul.anam@brainstation-23.com>
2024-08-27 16:11:31 +0600

    Initial Commit

---
+++ PhotogametryOnPhone/BackUpView.swift
@@ -0,0 +1,18 @@
+//
+//  BackUpView.swift
+//  PhotogametryOnPhone
+//
+//  Created by BS00834 on 23/8/24.
+//
+
+import SwiftUI
+
+struct BackUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackUpView()
+}
